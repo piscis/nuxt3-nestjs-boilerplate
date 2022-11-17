494e887ef3d902ea5bd3fc88d88718582a745734
Alex <gh@piscis.dev>
2022-11-17 22:24:02 +0100

    Updates nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  modules: [
+    "nuxt-security",
+    "@vueuse/nuxt",
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+  ],
+  tailwindcss: {
+    cssPath: "~/assets/css/main.css",
+    config: {
+      content: [],
+      theme: {
+        fontFamily: {
+          heading: ["Arial", "sans-serif"],
+          body: ["Roboto", "sans-serif"],
+        },
+        colors: {
+          primary: "#005138",
+          secondary: "#549973",
+          accent: "#ffe4bd",
+        },
+      },
+    },
+  },
+  runtimeConfig: {
+    authUser: "project",
+    authPass: "project",
+    environment: "develop",
+    buildAt: "",
+  },
+  telemetry: false,
+});
