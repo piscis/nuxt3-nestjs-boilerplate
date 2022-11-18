6e1f4ed53c070698e54c83d19ab49aa47fede0a4
Alexander Pirsig <self@pirsig.net>
2022-11-18 01:28:18 +0100

    update component

--- app.vue
+++ app.vue
@@ -3,6 +3,20 @@ useHead({
   title: "Hello, World!",
   titleTemplate: "%s - Nuxt3 Boilerplate",
 });
+
+const config = useRuntimeConfig();
+
+const {
+  data: serverNumber,
+  pending,
+  refresh,
+} = await useFetch("/api/randomNumber", {
+  baseURL: config.public.apiBase,
+});
+
+const fetchRandomNumber = async () => {
+  refresh();
+};
 </script>
 
 <template>
@@ -14,6 +28,18 @@ useHead({
           <b class="text-primary p-4 pt-2">Hello, World!</b>
         </h1>
         <p>Can you see me?</p>
+        <p>
+          Number:
+          <span v-if="pending">Loading ...</span>
+          <span else>{{ serverNumber }}</span>
+        </p>
+        <a
+          href="#"
+          class="mt-8 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600 hover:bg-indigo-50 sm:w-auto"
+          @click.prevent="fetchRandomNumber"
+        >
+          Fetch a number
+        </a>
       </div>
     </main>
   </div>
